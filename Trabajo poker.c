8f7cf4024bd6eb2e854fcd86dc8e551b03ce82e6
danizamarra <61791434+danizamarra@users.noreply.github.com>
2020-05-09 10:57:22 +0200

    Add files via upload

--- Trabajo poker.c
+++ Trabajo poker.c
@@ -27,10 +27,25 @@ typedef struct
 
 int main(){
 	FPresentacion ();
+	srand (time(NULL));
+	int i, j;
 	char valores[] = {'A', 'X', 'J', 'Q', 'K'};
 	char palo[] = {'C', 'P', 'D', 'T'};
 	Carta mano[] = {{valores[rand()%M],palo[rand()%N]},{valores[rand()%M],palo[rand()%N]},{valores[rand()%M],palo[rand()%N]},{valores[rand()%M],palo[rand()%N]},{valores[rand()%M],palo[rand()%N]}};
 	
+	//Este bucle evita que haya dos o mas cartas iguales en la misma mano 
+	
+	for (i=0; i<=M; i++) {
+        for (j=i+1; j<=M; j++){
+        	if ((mano[i].palo==mano[j].palo)&&(mano[i].valor==mano[j].valor)){
+        		//printf("Hay repeticion\n");
+        		do {			
+					mano[i].valor = valores[rand()%M];
+					
+				}while (mano[i].valor==mano[j].valor);
+			}
+		}
+    }
 	printf("Su mano es: %cde%c  %cde%c  %cde%c  %cde%c  %cde%c ",mano[0].valor, mano[0].palo,mano[1].valor, mano[1].palo,mano[2].valor, mano[2].palo,mano[3].valor, mano[3].palo,mano[4].valor, mano[4].palo);
 	
 	/*char c[10] = {"CORAZONES"};
